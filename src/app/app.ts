cc2b8d0d74a1e65f1403b38cf0ca3782e6a0cb13
Omnath Shinde <omnathshinde1@gmail.com>
2026-03-11 03:22:59 +0530

    Add Angular material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import { MatToolbar } from "@angular/material/toolbar";
 import { RouterOutlet } from "@angular/router";
 
 import { environment } from "src/environments/environment";
 
 @Component({
 	selector: "app-root",
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, MatToolbar, MatButtonModule, MatIcon],
 	templateUrl: "./app.html",
 	styleUrl: "./app.scss",
 })
